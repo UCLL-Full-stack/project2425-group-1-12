209c8ad71b58a7e0aab52d70db5d1aa3c2258a58
JorisNeetens <joris.neetens@student.ucll.be>
2024-12-18 02:52:55 +0100

    Eye password hider/unhider

--- front-end/components/Forms/loginForm.tsx
+++ front-end/components/Forms/loginForm.tsx
@@ -50,6 +50,7 @@ const LoginForm: React.FC = () => {
             title="Password:"
             label="Enter password"
             value={password}
+            secure={true}
             onChange={(e) => setPassword(e.target.value)}
             />
             <div className='buttons'>
