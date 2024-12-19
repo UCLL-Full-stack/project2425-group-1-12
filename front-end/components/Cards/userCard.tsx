f9abcbbf8389b50809694b0620fc72aeb75b7b2c
JorisNeetens <joris.neetens@student.ucll.be>
2024-12-19 18:55:14 +0100

    adminAccount&orderCard translations

--- front-end/components/Cards/userCard.tsx
+++ front-end/components/Cards/userCard.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from '../../styles/userCard.module.css';
 import { User } from '../../types/index';
+import { useTranslation } from 'next-i18next';
 
 type UserCardProps = {
     user: User;
 };
 
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
+    const { t } = useTranslation();
     return (
         <div className={styles.container}>
             <div className={styles.content}>
                 <h2>{user.name}</h2>
                 <p>Email: {user.email}</p>
-                <p>Address: {user.address}</p>
+                <p>{t('userCard.address')}{user.address}</p>
             </div>
         </div>
     );
